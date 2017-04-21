06d300b8f4150d2c7ebddf08aa30e5806920274f
Brabant Nicolas <nbr@ppgm.fr>
2017-04-22 00:50:43 +0200

    add parallax librairy

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -10,28 +10,20 @@
         <link rel="apple-touch-icon" href="apple-touch-icon.png">
         <!-- Place favicon.ico in the root directory -->
 
-        <link rel="stylesheet" type="text/css" href="/fonts.css" />
-        <link rel='stylesheet' type='text/css' href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:400,700'>
-        <link rel='stylesheet' type='text/css' href='/jquery.selectbox.css'>
-        <link rel='stylesheet' type='text/css' href='/perfect-scrollbar.min.css'>
-        <link rel='stylesheet' type='text/css' href="/app.css">
+        <link rel='stylesheet' type='text/css' href="/css/bootstrap.min.css">
+		<link rel='stylesheet' type='text/css' href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:400,700'>
+        <link rel='stylesheet' type='text/css' href="/css/app.css">
 
-        <link href="/components/require.css" rel="stylesheet" type="text/css">
-        <script src="/components/require.js"></script>
+		<script type="text/javascript" src="js/lib/jquery.min.js"></script>
+		<script type="text/javascript" src="js/lib/highlight.pack.js"></script>
+		<script type="text/javascript" src="js/lib/modernizr.custom.min.js"></script>
+        <script type="text/javascript" src="js/scrollmagic/uncompressed/ScrollMagic.js"></script>
     </head>
     <body>
 
         <div class='content'>
             @yield('content')
         </div>
-
-        <script>
-            require(['modernizr','jquery','jquery-ui'], function() {
-                requirejs(['/jquery.selectbox-0.2.min.js','/OAuthSimple.js','/perfect-scrollbar.jquery.min.js'], function () {
-                    requirejs(['/app.js']);
-                });
-            });
-        </script>
 
     </body>
 </html>
--- resources/views/parallax.blade.php
+++ resources/views/parallax.blade.php
@@ -2,6 +2,41 @@
 
 @section('content')
 
-    Hello the world!
+	<div class="scrollContent">
+
+		<script>
+			// init controller
+			var controller = new ScrollMagic.Controller();
+		</script>
+
+
+		<section id="header">
+
+		</section>
+
+		<section id="step_1" class="demo">
+			<div class="spacer s2"></div>
+			<div id="trigger1" class="spacer s0"></div>
+			<div id="animate1" class="box2 skin" style="background-color: green; transform: matrix(2.5, 0, 0, 2.5, 0, 0);">
+				<p>You wouldn't like me, when I'm angry!</p>
+				<a href="#" class="viewsource">view source</a>
+			</div>
+			<div class="spacer s2"></div>
+			<script>
+				// build scene
+				var scene = new ScrollMagic.Scene({
+					triggerElement: "#trigger1"
+				})
+				.setTween("#animate1", 0.5, {backgroundColor: "green", scale: 2.5}) // trigger a TweenMax.to tween
+				.addIndicators({name: "1 (duration: 0)"}) // add indicators (requires plugin)
+				.addTo(controller);
+			</script>
+		</section>
+
+		<section id="footer">
+
+		</section>
+
+	</div>
 
 @endsection
