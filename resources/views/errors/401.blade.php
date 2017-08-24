2fedc4f78c4446303774fe82020f98d78080fd6f
nbrabant <brabantnicolas59@gmail.com>
2017-08-24 23:36:42 +0200

    +1

--- resources/views/errors/401.blade.php
+++ resources/views/errors/401.blade.php
@@ -4,7 +4,6 @@
         <title>@lang('errors.error403').</title>
 
         <link href="https://fonts.googleapis.com/css?family=Roboto:200,400" rel="stylesheet" type="text/css">
-		<link href="{{ asset('la-assets/css/font-awesome.min.css') }}" rel="stylesheet" type="text/css" />
 
         <style>
             html, body {
@@ -48,14 +47,7 @@
         <div class="container">
             <div class="content">
 				<i class="fa fa-ban" style="font-size:120px;color:#FF5959;margin-bottom:30px;"></i>
-                <div class="title">@lang('errors.error403')</div>
-				@if(Auth::guest())
-					<a href="{{ url('/') }}">@lang('global.homepage')</a> |
-					<a href="javascript:history.back()">@lang('global.go_back')</a>
-				@else
-					<a href="{{ url(config('laraadmin.adminRoute')) }}">@lang('global.dashboard').</a> |
-					<a href="javascript:history.back()">@lang('global.go_back')</a>
-				@endif
+                <div class="title">@lang('errors.error401')</div>
             </div>
         </div>
     </body>
