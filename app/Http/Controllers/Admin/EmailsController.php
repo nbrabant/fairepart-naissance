97f61fc68e6394961c8b8ecf3f07df345dff9606
nbrabant <brabantnicolas59@gmail.com>
2017-08-27 23:02:23 +0200

    ajout fonctionnel édition de mail

--- app/Http/Controllers/Admin/EmailsController.php
+++ app/Http/Controllers/Admin/EmailsController.php
@@ -3,11 +3,19 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Email;
+use Validator;
 use Illuminate\Http\Request;
 use Laravel\Lumen\Routing\Controller as BaseController;
 
 class EmailsController extends BaseController
 {
+
+    protected $validation = [
+        'email'         => 'required|email|unique:emails,email',
+        'ip_address'    => 'ip',
+        'sended'        => 'boolean',
+        'readed'        => 'boolean',
+    ];
 
     public function index(Request $request)
     {
@@ -22,28 +30,67 @@ class EmailsController extends BaseController
             'sended' => 0,
             'readed' => 0,
         ]);
-        
+
         return view('admin.emails.create', compact('email'));
     }
 
     public function store(Request $request)
     {
+        $validator = Validator::make($request->all(), $this->validation);
 
+        if ($validator->fails()) {
+            $email = new Email([
+                'sended' => 0,
+                'readed' => 0,
+            ]);
 
+            return view('admin.emails.create', compact('email'));
+        }
+
+        $attributes = $request->only(array_keys($this->validation));
+
+        if (!isset($attributes['sended'])) {
+            $attributes['sended'] = 0;
+        }
+
+        if (!isset($attributes['readed'])) {
+            $attributes['readed'] = 0;
+        }
+
+        Email::create($attributes);
 
         return redirect('admin/emails');
     }
 
-    public function show(Request $request)
+    public function show(Request $request, $id = null)
     {
+        $email = Email::findOrFail($id);
 
-
-        return view('admin.emails.show');
+        return view('admin.emails.show', compact('email'));
     }
 
-    public function update(Request $request)
+    public function update(Request $request, $id = null)
     {
+        $email = Email::findOrFail($id);
 
+        $this->validation['email'] .= ',' . $id;
+        $validator = Validator::make($request->all(), $this->validation);
+
+        if ($validator->fails()) {
+            return view('admin.emails.show', compact('email'));
+        }
+
+        $attributes = $request->only(array_keys($this->validation));
+
+        if (!isset($attributes['sended'])) {
+            $attributes['sended'] = 0;
+        }
+
+        if (!isset($attributes['readed'])) {
+            $attributes['readed'] = 0;
+        }
+
+        $email->fill($attributes)->save();
 
         return redirect('admin/emails');
     }
---
+++ resources/views/admin/emails/show.blade.php
@@ -0,0 +1,57 @@
+@extends('admin.layout')
+
+@section('title')
+
+    <div class="page_header clearfix">
+        <div class="pull-left">
+            <h1>Emails - ajout</h1>
+        </div>
+    </div>
+
+@endsection
+
+@section('content')
+
+    <div class="page-content">
+
+        {!! Form::open(['url' => 'admin/emails/update/' . $email->id, 'class' => 'form-horizontal']) !!}
+
+            <div class="form-group">
+                {!! Form::label('email', 'Adresse E-mail', array('class' => 'col-md-3 control-label')) !!}
+                <div class="col-md-9">
+                    {!! Form::text('email', $email->email, array('class' => 'form-control')) !!}
+                </div>
+            </div>
+
+            <div class="form-group">
+                {!! Form::label('ip_address', 'Adresse IP', array('class' => 'col-md-3 control-label')) !!}
+                <div class="col-md-9">
+                    {!! Form::text('ip_address', $email->ip_address, array('class' => 'form-control')) !!}
+                </div>
+            </div>
+
+            <div class="form-group">
+                {!! Form::label('sended', 'Envoyé', array('class' => 'col-md-3 control-label')) !!}
+                <div class="col-md-9">
+                    {!! Form::checkbox('sended', $email->sended, true, array('class' => 'form-control')) !!}
+                </div>
+            </div>
+
+            <div class="form-group">
+                {!! Form::label('readed', 'Accédé', array('class' => 'col-md-3 control-label')) !!}
+                <div class="col-md-9">
+                    {!! Form::checkbox('readed', $email->readed, true, array('class' => 'form-control')) !!}
+                </div>
+            </div>
+
+            <div class="form-group">
+                <div class="col-md-9 col-md-offset-3">
+                    {!! Form::submit('Enregistrer', array('class' => 'btn btn-primary')) !!}
+                </div>
+            </div>
+
+        {!! Form::close() !!}
+
+    </div>
+
+@endsection
