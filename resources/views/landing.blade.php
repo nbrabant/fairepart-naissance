41553d31827f9d14fe45e4b36b99f8c69ac6738a
nbrabant <brabantnicolas59@gmail.com>
2017-08-28 22:30:55 +0200

    ajout texte landing page

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -11,7 +11,13 @@
             <img src="img/coming-soon.jpg" class="img-responsive" />
         </div>
 
-        email form
+        <p class="text-center">
+            Cette cignogne déposera bientôt dans nos bras un merveilleux cadeau.<br />
+            Celui-ci est si précieux qu'elle prendra le temps de venir,<br />
+            Dans cette attente, nous vous proposons de vous enregistrer afin de connaitre en temps cet heureux événement
+        </p>
+
+
     </div>
 
 @endsection
