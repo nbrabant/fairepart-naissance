a614200d183e031233fea239c617f9bc00ead007
nbrabant <brabantnicolas59@gmail.com>
2017-09-12 11:37:06 +0200

    up cron to every hours

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,6 +25,6 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('mailing')
-            ->cron('*/30 * * * *');
+            ->cron('* */1 * * *');
     }
 }
